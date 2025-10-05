9d2281ff4b115b7a87e24537993eb669665522a3
qiuyixiang <qiuyixiang2003@gmail.com>
2025-10-05 16:37:47 -0500

    add support for show completed books and details

--- src/modules/reading/userBook/dto/user-book-response.dto.ts
+++ src/modules/reading/userBook/dto/user-book-response.dto.ts
@@ -15,27 +15,45 @@ export class UserBookResponseDto {
   @ApiProperty({ description: "The user id of the user book" })
   user_id: string;
 
-  @ApiProperty({ description: "The status of the user book" })
+  @ApiProperty({
+    description: "The status of the user book",
+    enum: ReadingStatus,
+  })
   status: ReadingStatus;
 
-  @ApiProperty({ description: "The current page of the user book" })
+  @ApiProperty({
+    description: "The current page of the user book",
+    type: Number,
+  })
   current_page: number;
 
-  @ApiProperty({ description: "The start date of the user book" })
+  @ApiProperty({ description: "The start date of the user book", type: Date })
   start_date: Date | null;
 
-  @ApiProperty({ description: "The completed date of the user book" })
+  @ApiProperty({
+    description: "The completed date of the user book",
+    type: Date,
+  })
   completed_date: Date | null;
 
-  @ApiProperty({ description: "The total minutes of the user book" })
+  @ApiProperty({
+    description: "The total minutes of the user book",
+    type: Number,
+  })
   total_minutes: number;
 
-  @ApiProperty({ description: "The total days of the user book" })
+  @ApiProperty({ description: "The total days of the user book", type: Number })
   total_days: number;
 
-  @ApiProperty({ description: "The created at date of the user book" })
+  @ApiProperty({
+    description: "The created at date of the user book",
+    type: Date,
+  })
   createdAt: Date;
 
-  @ApiProperty({ description: "The updated at date of the user book" })
+  @ApiProperty({
+    description: "The updated at date of the user book",
+    type: Date,
+  })
   updatedAt: Date;
 }
